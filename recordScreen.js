21dcb0ee26b5cfe77328e36ee99ef9c3578c8072
gaurezzz <gaurez@outlook.com>
2024-09-22 17:03:24 -0600

    cambio url

--- recordScreen.js
+++ recordScreen.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
